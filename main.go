262ee933389caa5e3b9525951cab575a9a1905ff
Akira Chiku <akira.chiku@gmail.com>
2021-10-20 21:21:18 +0900

    Update

--- main.go
+++ main.go
@@ -5,7 +5,10 @@ package main
 import (
 	"context"
 	"database/sql"
+	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"time"
 
 	"github.com/jackc/pgx"
@@ -56,6 +59,72 @@ type DBer interface {
 	Ping() error
 }
 
+// App application
+type App struct {
+	DB DBer
+}
+
+func newTimeoutMiddleware(timeout time.Duration) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx, cancel := context.WithTimeout(r.Context(), timeout)
+			defer cancel()
+			req := r.WithContext(ctx)
+			next.ServeHTTP(w, req)
+		})
+	}
+}
+
+func (app *App) hello(w http.ResponseWriter, r *http.Request) {
+	log.Println("hello access")
+	fmt.Fprintf(w, "hello\n")
+	return
+}
+
+func (app *App) slowHello(w http.ResponseWriter, r *http.Request) {
+	log.Println("slow hello access")
+	for i := 0; i < 5; i++ {
+		time.Sleep(1 * time.Second)
+		log.Printf("waiting %d...", i)
+	}
+	fmt.Fprintf(w, "slow hello\n")
+	return
+}
+
+func (app *App) slowHelloDB(w http.ResponseWriter, r *http.Request) {
+	log.Println("slow hello db access")
+	var val interface{}
+	err := app.DB.QueryRowContext(r.Context(), "select pg_sleep(5)").Scan(&val)
+	if err != nil {
+		log.Println("query failed")
+		return
+	}
+	fmt.Fprintf(w, "slow hello db\n")
+	return
+}
+
+var (
+	uri = flag.String("dburi", "", "database uri")
+)
+
 func main() {
-	fmt.Println("query cancel test")
+	flag.Parse()
+
+	db, err := NewDB(*uri)
+	if err != nil {
+		log.Fatal(err)
+	}
+	app := App{DB: db}
+
+	timeout := newTimeoutMiddleware(1 * time.Second)
+	mux := http.NewServeMux()
+	mux.Handle("/hello", http.HandlerFunc(app.hello))
+	mux.Handle("/slow-hello", http.HandlerFunc(app.slowHello))
+	mux.Handle("/timeout-hello-custom", timeout(http.HandlerFunc(app.slowHello)))
+	mux.Handle("/timeout-hello-stdlib", http.TimeoutHandler(http.HandlerFunc(app.slowHello), 2*time.Second, "timeout!!\n"))
+	mux.Handle("/timeout-hello-db", http.TimeoutHandler(http.HandlerFunc(app.slowHelloDB), 2*time.Second, "timeout!!\n"))
+
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
 }
